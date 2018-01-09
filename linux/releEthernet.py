5124f3dc31601df1f39135c1da7b83bbd05171b6
Diego Rocha <diego@diegorocha.com.br>
2018-01-09 19:09:48 -0200

    Ajustes de pep8 no client linux

--- linux/releEthernet.py
+++ linux/releEthernet.py
@@ -2,10 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 from __future__ import print_function
-from json import loads
+
+from urllib.parse import urljoin
+
 from requests import get
 from decouple import config
-from argparse import ArgumentParser, Action
+from argparse import ArgumentParser
 
 try:
     import gi
@@ -17,61 +19,63 @@ except:
 
 
 class ReleEthernet(object):
-    """ This class is a interface between Python and the Arduino to controls the relays. """
-
-    __arduino_ip__ = config('arduino_ip', default='192.168.1.118')
-    __arduino_host_name__ = config('arduino_host_name', default='http://%s' % (__arduino_ip__))
+    """ This class is a interface between Python and the Arduino to controls
+    the relays. """
 
     def __init__(self):
-        self.conectado = self.testarComunicacao()
+        self.conectado = self.testar_comunicacao()
+        self.arduino_ip = config('arduino_ip', default='192.168.0.16')
+        self.arduino_host_name = config('arduino_host_name',
+                                        default='http://%s' % self.arduino_ip)
 
     """
     Return the correct url to perform a command on a relay.
     """
 
-    def _getUrl(self, rele=0, comando=1):
+    def _get_url(self, rele=0, comando=1):
         if comando == '?':
             # Acerta o parse da URL incluindo um parâmetro qualquer
             comando = '?foo=bar'
-        return '%s/%d%s' % (self.__arduino_host_name__, rele, comando)
+        path = '/' + str(rele) + comando
+        return urljoin(self.arduino_host_name, path)
 
     """
     Send a HTTP GET with a command to perform on a relay to Arduino.
     """
 
-    def enviarComando(self, rele=0, comando='1', showReturn=False):
-        url = self._getUrl(rele, comando)
+    def enviar_comando(self, rele=0, comando='1', show_return=False):
+        url = self._get_url(rele, comando)
         r = get(url)
-        estado = loads(r.text)
-        if showReturn:
+        estado = r.json()
+        if show_return:
             print(estado)
         return estado
 
-    def testarComunicacao(self):
+    def testar_comunicacao(self):
         """ Try to send a command to device. """
         try:
-            self.enviarComando(0, '?')
+            self.enviar_comando(0, '?')
             return True
         except Exception:
             return False
 
     def ligar(self, rele=0):
         """ Turn on a relay. """
-        return self.enviarComando(rele, '1')
+        return self.enviar_comando(rele, '1')
 
     def desligar(self, rele=0):
         """ Turn off a relay. """
-        return self.enviarComando(rele, '0')
+        return self.enviar_comando(rele, '0')
 
     def inverter(self, rele=0):
         """ Switch the state of a relay. """
-        return self.enviarComando(rele, '!')
+        return self.enviar_comando(rele, '!')
 
     def estado(self, rele=0):
         """ Ask Arduino the state of a relay. """
-        return self.enviarComando(rele, '?')
+        return self.enviar_comando(rele, '?')
 
-    def releLigado(self, rele=0):
+    def rele_ligado(self, rele=0):
         """ Return true if the given relay is on, false if relay is off. Otherwise
         raise a Exception. """
         estado = self.estado(rele)
@@ -79,7 +83,7 @@ class ReleEthernet(object):
         if erro:
             raise Exception(erro)
         return estado["rele"]
-pass
+
 
 if gtk_loaded:
     class GuiWindow(Gtk.Window):
@@ -87,9 +91,9 @@ if gtk_loaded:
         This class is a grapic user interface to control the relays.
         """
 
-        def __init__(self, ReleEthernet):
+        def __init__(self, rele_ethernet):
             Gtk.Window.__init__(self, title='Rele Ethernet')
-            self.releEthernet = ReleEthernet
+            self.rele_ethernet = rele_ethernet
             self.set_border_width(10)
             self.set_position(Gtk.WindowPosition.CENTER)
             self.set_default_size(200, 0)
@@ -113,7 +117,7 @@ if gtk_loaded:
 
             switch = Gtk.Switch()
             switch.props.valign = Gtk.Align.CENTER
-            switch.set_active(self.releEthernet.releLigado())
+            switch.set_active(self.rele_ethernet.rele_ligado())
             switch.connect('notify::active', self.on_switch_activate, 0)
             hbox.pack_start(switch, False, True, 0)
 
@@ -121,16 +125,15 @@ if gtk_loaded:
 
         def on_switch_activate(self, widget, gparam, rele):
             if widget.get_active():
-                self.releEthernet.ligar()
+                self.rele_ethernet.ligar()
             else:
-                self.releEthernet.desligar()
-    pass
+                self.rele_ethernet.desligar()
 
 
 def main():
-    argumentParser = ArgumentParser(
+    argument_parser = ArgumentParser(
         epilog='Author: Diego Rocha <diego@diegorocha.com.br>')
-    sub_parser = argumentParser.add_subparsers()
+    sub_parser = argument_parser.add_subparsers()
 
     on_parser = sub_parser.add_parser('on', help='turn on a relay')
     on_parser.set_defaults(command='1', gui=False)
@@ -149,11 +152,14 @@ def main():
         'gui', help='show a grapic user interface')
     gui_parser.set_defaults(command='', gui=True)
 
-    args = argumentParser.parse_args()
+    args = argument_parser.parse_args()
     rele = ReleEthernet()
+    if not args.__dict__:
+        print("Two few arguments")
+        exit(1)
     if args.gui:
         if gtk_loaded:
-            win = GuiWindow(ReleEthernet=rele)
+            win = GuiWindow(rele_ethernet=rele)
             win.connect("delete-event", Gtk.main_quit)
             win.show_all()
             Gtk.main()
@@ -161,11 +167,12 @@ def main():
             print('Could not load GTK')
     else:
         if rele.conectado:
-            estado = rele.enviarComando(comando=args.command)
+            estado = rele.enviar_comando(comando=args.command)
             if '?' in args.command:
                 print(estado['rele'])
         else:
             print('Arduino Not Found')
+
 
 if __name__ == '__main__':
     main()
