6c6926f57026fba5c6571333edae6aad1134ce7c
Diego Rocha <diego@diegorocha.com.br>
2018-07-13 21:19:01 -0300

    Ajustes no cache do ajax

--- raspberry/app.py
+++ raspberry/app.py
@@ -1,4 +1,5 @@
 from datetime import timedelta, datetime
+from functools import wraps
 
 from flask import Flask, render_template
 from flask.json import jsonify
@@ -8,26 +9,23 @@ from pi import ReleInterface
 app = Flask(__name__)
 
 
+def nocache(view):
+    @wraps(view)
+    def decorated(*args, **kwargs):
+        response = view(*args, **kwargs)
+        response.cache_control.max_age = 0
+        return response
+    return decorated
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/api/rele')
-@app.route('/api/rele/<action>', methods=['POST'])
-def api(action=''):
-    rele = ReleInterface()
-    error = ''
-    if action:
-        try:
-            getattr(rele, action)()
-        except:
-            error = 'Não foi possível executar a operação'
-    return jsonify({'rele': rele.is_on, 'error': error})
-
-
 @app.route("/0", defaults={'command': '?'})
 @app.route("/0<command>")
+@nocache
 def api_compatibilidade(command):
     command_map = {'0': 'off', '1': 'on', '!': 'toggle', '?': ''}
     action = command_map.get(command)
@@ -36,7 +34,7 @@ def api_compatibilidade(command):
 
 @app.after_request
 def set_cache_headers(response):
-    default_max_age = 3600
+    default_max_age = 7200
     if app.debug:
         response.cache_control.max_age = 0
     if response.cache_control.max_age == 0:
@@ -48,6 +46,20 @@ def set_cache_headers(response):
     if not response.expires:
         response.expires = datetime.utcnow() + timedelta(seconds=response.cache_control.max_age)
     return response
+
+
+@app.route('/api/rele')
+@app.route('/api/rele/<action>', methods=['POST'])
+@nocache
+def api(action=''):
+    rele = ReleInterface()
+    error = ''
+    if action:
+        try:
+            getattr(rele, action)()
+        except:
+            error = 'Não foi possível executar a operação'
+    return jsonify({'rele': rele.is_on, 'error': error})
 
 
 if __name__ == "__main__":
